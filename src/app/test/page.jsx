db9bea5a98c9fdef840a949175aa439277fc89c3
Mursalin <mursalin2311@gmail.com>
2024-10-04 00:24:38 +0600

    will work on ggole auth later

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,8 +1,11 @@
 import GoogleLogin from "@/components/Auth/googleLogin";
 
 const TestPage = () => {
+  
+  // I am planing that auth will be implement at last ok
   return (
     <div>
+      
       <GoogleLogin />
     </div>
   );
