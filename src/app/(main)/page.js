0cdc35af33ea25656ab135702fbf3c74830f010f
Mursalin <mursalin2311@gmail.com>
2024-10-14 23:23:46 +0600

    will work for responsive now

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -2,7 +2,7 @@ import HomePge from "@/components/page-specific/home/HomePage/HomePge";
 
 const page = () => {
   return (
-    <div>
+    <div className="border-2 border-red-700">
       <HomePge />
     </div>
   );
