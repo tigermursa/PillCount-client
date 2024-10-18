2c65607295e766e6822aa63ec18d238cc7e5f445
Mursalin <mursalin2311@gmail.com>
2024-10-18 21:29:13 +0600

    going to updatelittle

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -2,7 +2,7 @@ import HomePge from "@/components/page-specific/home/HomePage/HomePge";
 
 const page = () => {
   return (
-    <div className="border-2 border-red-700">
+    <div className="">
       <HomePge />
     </div>
   );
